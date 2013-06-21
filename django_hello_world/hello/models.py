5155976325acf41b2b5caa31cafdd037bb97db05
Shurik <stu.shurik@gmail.com>
2013-06-21 17:17:57 +0300

    ticket:1 initial commit for south

--- django_hello_world/hello/models.py
+++ django_hello_world/hello/models.py
@@ -1,3 +1,14 @@
+from django.utils.timezone import now
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User)
+    birthdate = models.DateField(default=now())
+    bio = models.TextField(blank=True)
+    contacts = models.CharField(blank=True, max_length=255)
+
+    def __unicode__(self):
+        return self.user.email
