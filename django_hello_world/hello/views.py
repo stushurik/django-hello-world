091d131a0a10fd637cf1380965bfd71f17f01b21
Shurik <stu.shurik@gmail.com>
2013-06-21 20:08:33 +0300

    ticket:1 last modification in test, make simple tamplate, and view wich process request

--- django_hello_world/hello/views.py
+++ django_hello_world/hello/views.py
@@ -1,8 +1,23 @@
-from annoying.decorators import render_to
 from django.contrib.auth.models import User
+from django.views.generic import TemplateView
 
 
-@render_to('hello/home.html')
-def home(request):
-    users = User.objects.filter()
-    return {'users': users}
+class IndexView(TemplateView):
+
+    template_name = 'hello/home.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(IndexView, self).get_context_data(**kwargs)
+        user = User.objects.get(email='stu.shurik@gmail.com')
+        data = (user.first_name,
+                user.last_name,
+                user.userprofile.birthday,
+                user.userprofile.bio,
+                user.email,
+                user.userprofile.contacts,
+                user.userprofile.jabber,
+                user.userprofile.skype,
+                user.userprofile.other,
+        )
+        context['data'] = data
+        return context
