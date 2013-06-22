3b983291ff95484e975353576194fe4bbe95a099
Shurik <stu.shurik@gmail.com>
2013-06-22 12:39:32 +0300

    ticket:1 edit main page

--- django_hello_world/hello/views.py
+++ django_hello_world/hello/views.py
@@ -9,15 +9,13 @@ class IndexView(TemplateView):
     def get_context_data(self, **kwargs):
         context = super(IndexView, self).get_context_data(**kwargs)
         user = User.objects.get(email='stu.shurik@gmail.com')
-        data = (user.first_name,
-                user.last_name,
-                user.userprofile.birthday,
-                user.userprofile.bio,
-                user.email,
-                user.userprofile.contacts,
-                user.userprofile.jabber,
-                user.userprofile.skype,
-                user.userprofile.other,
-        )
-        context['data'] = data
+        context['first_name'] = user.first_name
+        context['last_name'] = user.last_name
+        context['birthday'] = user.userprofile.birthday
+        context['bio'] = user.userprofile.bio
+        context['email'] = user.email
+        context['contacts'] = user.userprofile.contacts
+        context['jabber'] = user.userprofile.jabber
+        context['skype'] = user.userprofile.skype
+        context['other'] = user.userprofile.other
         return context
