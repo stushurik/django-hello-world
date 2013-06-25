2f6696265a15500cb5c36e1c5ab5a66d66099e0a
Shurik <stu.shurik@gmail.com>
2013-06-25 16:57:15 +0300

    refactoring

---
+++ django_hello_world/static/js/edit_page.js
@@ -0,0 +1,53 @@
+$(document).ready(function() {
+    var options = {
+        beforeSubmit:function () {
+                $("#loader").css("display", "block");
+                return true;
+        },
+        success:function(responseText)  {
+                $("#loader").css("display", "none");
+                $('#user_data_form').find('input, textarea, button').removeAttr('disabled');
+        }
+    };
+
+    $('#id_uploaded_file').change(function(){
+        var data = new FormData();
+        $.each($('#id_uploaded_file')[0].files, function(i, file) {
+                data.append('uploaded_file', file);
+        });
+
+        data.append('csrfmiddlewaretoken','{{ csrf_token }}');
+
+        $.ajax({
+            url: '/profile/upload_file/',
+            data: data,
+            cache: false,
+            contentType: false,
+            processData: false,
+            type: 'POST',
+            success: function(data){
+                $('.img-polaroid').attr('src',data);
+            }
+        });
+    });
+
+    $('#close').click(function(){
+        $.ajax({
+            type: 'POST',
+            url: '/profile/delete_file/',
+            data:{ csrfmiddlewaretoken: '{{ csrf_token }}'},
+            success: function(data){
+                $('#id_uploaded_file').val('');
+                $('.img-polaroid').attr('src',data);
+            }
+        });
+    });
+
+
+
+    $('#user_data_form').submit(function() {
+        $(this).ajaxSubmit(options);
+        $('#user_data_form').find('input, textarea, button').attr('disabled','disabled');
+        return false;
+     });
+});
