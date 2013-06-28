51dcbfb10c79c681207bd9695e3c395b08ae9bd6
Shurik <stu.shurik@gmail.com>
2013-06-28 18:27:34 +0300

    refactoring

--- django_hello_world/hello/tests.py
+++ django_hello_world/hello/tests.py
@@ -4,13 +4,11 @@ when you run "manage.py test".
 
 Replace this with more appropriate tests for your application.
 """
-from django.conf import settings
 from django.contrib.auth.models import User
 
 from django.core.urlresolvers import reverse
 from django.test import TestCase
 from django.test.client import Client
-from django_hello_world.hello.models import UserProfile, WebRequest
 
 
 class SimpleTest(TestCase):
@@ -22,9 +20,10 @@ class SimpleTest(TestCase):
 
 
 class HttpTest(TestCase):
+    fixtures = ['initial_data.json']
+
     def test_home(self):
         admin = User.objects.get(email='stu.shurik@gmail.com')
-        profile = UserProfile.objects.get(user=admin)
 
         c = Client()
         response = c.get(reverse('home'))
@@ -39,29 +38,29 @@ class HttpTest(TestCase):
         self.assertEqual("Poplavskyi", admin.last_name)
 
         self.assertContains(response, 'Date of birth')
-        self.assertContains(response, profile.birthday)
-        self.assertEqual("1992-06-19", str(profile.birthday))
+        self.assertContains(response, admin.userprofile.birthday)
+        self.assertEqual("1992-06-19", str(admin.userprofile.birthday))
 
         self.assertContains(response, 'Bio')
-        self.assertContains(response, profile.bio)
-        self.assertEqual('student of the CSTU', profile.bio)
+        self.assertContains(response, admin.userprofile.bio)
+        self.assertEqual('student of the CSTU', admin.userprofile.bio)
 
         self.assertContains(response, 'Email')
         self.assertContains(response, admin.email)
         self.assertEqual("stu.shurik@gmail.com", admin.email)
 
         self.assertContains(response, 'Contacts')
-        self.assertContains(response, profile.contacts)
-        self.assertEqual("Chernigiv, Dotsenko str. 12 app. 17", profile.contacts)
+        self.assertContains(response, admin.userprofile.contacts)
+        self.assertEqual("Chernigiv, Dotsenko str. 12 app. 17", admin.userprofile.contacts)
 
         self.assertContains(response, 'Jabber')
-        self.assertContains(response, profile.jabber)
-        self.assertEqual("stushurik@khavr.com", profile.jabber)
+        self.assertContains(response, admin.userprofile.jabber)
+        self.assertEqual("stushurik@khavr.com", admin.userprofile.jabber)
 
         self.assertContains(response, 'Skype')
-        self.assertContains(response, profile.skype)
-        self.assertEqual("shurik.poplavskyi", profile.skype)
+        self.assertContains(response, admin.userprofile.skype)
+        self.assertEqual("shurik.poplavskyi", admin.userprofile.skype)
 
         self.assertContains(response, 'Other contacts')
-        self.assertContains(response, profile.other)
-        self.assertEqual("-", profile.other)
+        self.assertContains(response, admin.userprofile.other)
+        self.assertEqual("-", admin.userprofile.other)
--- django_hello_world/settings.py
+++ django_hello_world/settings.py
@@ -1,4 +1,5 @@
 # Django settings for django_hello_world project.
+import os
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
@@ -43,6 +44,8 @@ USE_I18N = True
 # calendars according to the current locale
 USE_L10N = True
 
+PROJECT_PATH = os.path.realpath(os.path.dirname(__file__))
+
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
 MEDIA_ROOT = ''
@@ -56,7 +59,7 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = os.path.join(PROJECT_PATH, 'static')
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
