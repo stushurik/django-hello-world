92a3f0d507d5f3310ea8257a96e469223949938e
Shurik <stu.shurik@gmail.com>
2013-06-28 22:11:40 +0300

    ticket:10 refactoring

--- django_hello_world/settings.py
+++ django_hello_world/settings.py
@@ -108,7 +108,6 @@ MIDDLEWARE_CLASSES = (
 ROOT_URLCONF = 'django_hello_world.urls'
 
 TEMPLATE_DIRS = (
-    os.path.join(PROJECT_PATH, 'hello/templates'),
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
