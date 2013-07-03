96d8315031c59b3a262cb229d22fa1a43fe1ce45
Shurik <stu.shurik@gmail.com>
2013-07-03 13:04:30 +0300

    ticket:10 change model signals callback functions

--- django_hello_world/hello/admin.py
+++ django_hello_world/hello/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from django_hello_world.hello.models import UserProfile, WebRequest
+
+from django_hello_world.hello.models import UserProfile, WebRequest, ModelsOperation
 
 
 admin.site.register(UserProfile)
 admin.site.register(WebRequest)
+admin.site.register(ModelsOperation)
